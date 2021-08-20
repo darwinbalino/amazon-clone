fffd7ff035ca957b7e10c8ddaf0223562ace79d8
darwinbalino <darwinbalino@gmail.com>
2021-08-20 12:42:12 -0700

    built front-end

---
+++ components/Product.js
@@ -0,0 +1,51 @@
+import { StarIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import React, { useState } from "react";
+import Currency from "react-currency-formatter";
+
+const MIN_RATING = 1;
+const MAX_RATING = 5;
+
+const Product = ({ id, title, price, description, category, image }) => {
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+  const [hasPrime] = useState(Math.random() < 0.5);
+
+  return (
+    <div className="relative z-30 flex flex-col p-10 m-5 bg-white">
+      <p className="absolute text-xs italic text-gray-400 top-2 right-2">
+        {category}
+      </p>
+
+      <Image src={image} height={200} width={200} objectFit="contain" />
+
+      <h4 className="my-3">{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500" />
+          ))}
+      </div>
+
+      <p className="my-2 text-xs line-clamp-2">{description}</p>
+
+      <div className="mb-5">
+        <Currency quantity={price} currency="CAD" />
+      </div>
+
+      {hasPrime && (
+        <div className="flex items-center -mt-5 space-x-2">
+          <img className="w-12" src="https://links.papareact.com/fdw" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+        </div>
+      )}
+
+      <button className="mt-auto button">Add to Basket</button>
+    </div>
+  );
+};
+
+export default Product;
